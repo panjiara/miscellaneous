0ff6b849dd1683821777055f90adf5180c5fc87c
saurabh kumar <itsmeonly06@gmail.com>
2017-02-09 21:21:56 +0530

    2's complement of a string
    finding the 2's complement of a binary string

---
+++ 2's.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;\
+int main()
+{
+    string s;
+    cin>>s;
+    int l=s.length();
+    int i;
+    for(i=l-1;i>=0;i--)
+    {
+        if(s[i]=='1')
+            break;
+    }
+    if(i+1==0)
+    {
+      cout<<"happy";
+
+    }
+    else
+    {
+      for(int k=i-1;k>=0;k--)
+           {
+               if(s[k]=='1')
+                s[k]='0';
+               else
+                s[k]='1';
+           }
+    cout<<s<<endl;
+
+    }
+
+
+
+    return 0;
+
+}
